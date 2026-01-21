0054e90a9e2a513fb8905160c563ae6ed3fcd1e9
nhzlf <nhzlf@163.com>
2026-01-21 15:43:45 +0800

    更新 config.rs

--- src/config.rs
+++ src/config.rs
@@ -8,7 +8,7 @@ use std::{
     sync::{Mutex, RwLock},
     time::{Duration, Instant, SystemTime},
 };
-//20260109-20260121-2026-1-2-3-4-5-6-7-8
+//20260109-20260121
 use anyhow::Result;
 use bytes::Bytes;
 use rand::Rng;
